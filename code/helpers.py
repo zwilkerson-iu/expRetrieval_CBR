7d000b1531025f3418746e4b85a4a9262935cbae
zwilk314 <zwilkerson314@gmail.com>
2021-03-11 20:34:22 -0500

    prepping for tests

--- code/helpers.py
+++ code/helpers.py
@@ -172,7 +172,7 @@ def runTests(numIterations:int, features:int, examplesPerAnimal:int, rootDir:str
     results["stdev"] = std
     print("Average:", ave)
     print("Standard deviation:", std)
-    record = open("../csvFiles/" + str(featureSelectionMode) + "_" + str(randomBound) + "_" + str(weightsUsed) + "_" + str(k) + "_results.csv", "w")
+    record = open("../results/" + str(featureSelectionMode) + "_" + str(randomBound) + "_" + str(weightsUsed) + "_" + str(k) + "_results.csv", "w")
     for iteration in results.keys():
         record.write(str(iteration) + "," + str(results[iteration]) + "\n")
     record.close()
