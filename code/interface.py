4fab9fb1b4b5da26208c5fe3b5b653b862824480
zwilk314 <zwilkerson314@gmail.com>
2021-03-16 21:40:06 -0400

    test ready???

--- code/interface.py
+++ code/interface.py
@@ -17,7 +17,7 @@ def run(runningSystem:str):
         rootDir = "../../expRetrieval_CBR_data/"
     else: #remote
         rootDir = "/l/vision/magnemite/expRetrieval_CBR_data/" #***Need to command "conda activate tensorflow" before running this mode
-    NUMITERATIONS = 30
+    NUMITERATIONS = 2
 
     print("Ready for command:")
 
