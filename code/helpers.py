91fe73bfb5c14ebe16e7cd21fb9aea707e439528
zwilk314 <zwilkerson314@gmail.com>
2021-04-13 18:49:38 -0400

    extra stdev tests?

--- code/helpers.py
+++ code/helpers.py
@@ -207,8 +207,8 @@ def runTests(numIterations:tuple, features:int, examplesPerAnimal:int, rootDir:s
             randStart = 1
             randEnd = randomBound
         else:
-            # randStart = randomBound
-            randStart = 2 #TODO: revert!!!
+            randStart = randomBound
+            # randStart = 2 #TODO: revert!!!
             randEnd = randomBound+1
         for randomness in range(randStart, randEnd):
             results[k][randomness] = []
